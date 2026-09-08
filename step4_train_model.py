7a45f822f7b1de7c198603f3d78601e6fdb8d0ad
Jonathan Lai <jlai9001@gmail.com>
2026-09-07 23:02:04 -0700

    training expanded

--- step4_train_model.py
+++ step4_train_model.py
@@ -196,7 +196,7 @@ class CodecTransformer(nn.Module):
 def train_model(
     training_dataset_path,
     output_path="outputs/codec_transformer.pt",
-    epochs=250,
+    epochs=150,
     batch_size=32,
 ):
     # Choose the GPU when CUDA is available
